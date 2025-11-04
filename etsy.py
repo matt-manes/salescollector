1495d62d0e345d57e206cd329f9612ef37fbfe32
matt manes <mattmanes@pm.me>
2025-11-04 12:42:39 -0600

    refactor: convert etsy functions to classes

--- etsy.py
+++ etsy.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime, timedelta
+from functools import cached_property
 from typing import Any, cast
 
 import loggi
@@ -11,6 +12,7 @@ from etsy_python.v3.resources import ReceiptResource  # type: ignore
 from etsy_python.v3.resources import Response, UserResource  # type: ignore
 from etsy_python.v3.resources.Session import EtsyClient  # type: ignore
 from pathier import Pathier
+from typing_extensions import Self
 
 import exceptions
 from db import SCDatabased
@@ -22,127 +24,140 @@ def get_logger() -> loggi.Logger:
     return loggi.getLogger("etsy", root / "logs")
 
 
-def get_oauth() -> EtsyOAuth:
-    key: str | None = os.getenv("sc_keystring")
-    redirect: str | None = os.getenv("sc_oauth-redirect")
-    if key is None or redirect is None:
-        raise exceptions.MissingEnvException("Missing sc_* keys in .env")
-    return EtsyOAuth(
-        keystring=key,
-        redirect_uri=redirect,
-        scopes=["transactions_r", "shops_r"],
-    )
-
-
-def get_client(token_data: dict[str, str]) -> EtsyClient:
-    key: str | None = os.getenv("sc_keystring")
-    if not key:
-        raise exceptions.MissingEnvException("Missing sc_* keys in .env")
-    return EtsyClient(
-        keystring=key,
-        access_token=token_data["access_token"],
-        refresh_token=token_data["refresh_token"],
-        expiry=datetime.now() + timedelta(seconds=int(token_data["expires_in"])),
-    )
-
-
-def get_authenticated_client(code: str, state: str) -> EtsyClient:
-    oauth: EtsyOAuth = get_oauth()
-    with SCDatabased() as db:
-        session: dict[str, str] = db.get_oauth(state)
-    oauth.state = session["state"]
-    oauth.code_verifier = session["code_verifier"]
-    oauth.set_authorisation_code(code=code, state=state)
-    token_data: dict[str, str] = cast(dict[str, str], oauth.get_access_token())
-    return get_client(token_data)
-
-
-def state_exists(state: str) -> bool:
-    with SCDatabased() as db:
-        return db.state_exists(state)
-
-
-def save_oauth(state: str, code_verifier: str) -> None:
-    with SCDatabased() as db:
-        db.register_oauth(state, code_verifier)
-
-
 def log_and_raise_api_error(message: str) -> exceptions.APIException:
     get_logger().error(message)
     raise exceptions.APIException(message)
 
 
-def get_shop_id(client: EtsyClient) -> str:
-    user: UserResource = UserResource(client)
-    try:
-        # etsy_python mistyped resource objects to either return
-        # the resource or a `RequestException`, but the code
-        # for `Session` actually raises the exception or
-        # returns the response smh
-        response: Response = cast(Response, user.get_me())
-    except RequestException as e:
-        log_and_raise_api_error(f"Getting shop id failed: {e}")
-    # type ignore b/c etsy_python mistyped `message` field as `str`
-    # even though it's `dict[str, Any]` for any successful request that returns data
-    return response.message["shop_id"]  # type: ignore
+class OAuthProvider:
+    @staticmethod
+    def get_new_oauth() -> EtsyOAuth:
+        key: str | None = os.getenv("sc_keystring")
+        redirect: str | None = os.getenv("sc_oauth-redirect")
+        if key is None or redirect is None:
+            raise exceptions.MissingEnvException("Missing sc_* keys in .env")
+        return EtsyOAuth(
+            keystring=key,
+            redirect_uri=redirect,
+            scopes=["transactions_r", "shops_r"],
+        )
+
+    @staticmethod
+    def get_auth_url() -> str:
+        oauth: EtsyOAuth = OAuthProvider.get_new_oauth()
+        auth_url: str = oauth.get_auth_code()[0]
+        OAuthProvider.save_state(oauth)
+        return auth_url
+
+    @staticmethod
+    def state_exists(state: str) -> bool:
+        with SCDatabased() as db:
+            return db.state_exists(state)
+
+    @staticmethod
+    def save_state(oauth_instance: EtsyOAuth) -> None:
+        with SCDatabased() as db:
+            db.register_oauth(oauth_instance.state, oauth_instance.code_verifier)
+
+    @staticmethod
+    def get_saved_oauth(state: str) -> EtsyOAuth:
+        oauth: EtsyOAuth = OAuthProvider.get_new_oauth()
+        with SCDatabased() as db:
+            session: dict[str, str] = db.get_oauth(state)
+        oauth.state = session["state"]
+        oauth.code_verifier = session["code_verifier"]
+        return oauth
 
 
-def get_shop_sales_data(client: EtsyClient, shop_id: str) -> list[dict[str, Any]]:
-    receipts: ReceiptResource = ReceiptResource(client)
-    offset: int = 0
-    limit: int = 100
-    total: int = -1
-    results: list[dict[str, Any]] = []
-    # don't know total until we get the first response
-    while total == -1 or len(results) < total:
+class AuthenticatedClient:
+    def __init__(self, token_data: dict[str, str]) -> None:
+        key: str | None = os.getenv("sc_keystring")
+        self._shop_id: str | None = None
+        if not key:
+            raise exceptions.MissingEnvException("Missing sc_* keys in .env")
+        self.client: EtsyClient = EtsyClient(
+            keystring=key,
+            access_token=token_data["access_token"],
+            refresh_token=token_data["refresh_token"],
+            expiry=datetime.now() + timedelta(seconds=int(token_data["expires_in"])),
+        )
+
+    @classmethod
+    def from_redirect(cls, code: str, state: str) -> Self:
+        oauth: EtsyOAuth = OAuthProvider.get_saved_oauth(state)
+        oauth.set_authorisation_code(code=code, state=state)
+        token_data: dict[str, str] = cast(dict[str, str], oauth.get_access_token())
+        return cls(token_data)
+
+    @cached_property
+    def shop_id(self) -> str:
+        user: UserResource = UserResource(self.client)
         try:
-            response: Response = cast(
-                Response,
-                receipts.get_shop_receipts(int(shop_id), limit=limit, offset=offset),
-            )
+            # etsy_python mistyped resource objects to either return
+            # the resource or a `RequestException`, but the code
+            # for `Session` actually raises the exception or
+            # returns the response smh
+            response: Response = cast(Response, user.get_me())
         except RequestException as e:
-            message = f"Failure to get receipts for shop id '{shop_id}'\n{offset=}\n{total=}\n{len(results)=}\n{e}"
-            log_and_raise_api_error(message)
-        else:
-            # etsy_python mistyped `message` field as `str`
-            # even though it's `dict[str, Any]` for any successful request that returns data
-            data: dict[str, Any] = cast(dict[str, Any], response.message)
-            total = data["count"]
-            results.extend(data["results"])
-            offset += limit
-    return results
+            log_and_raise_api_error(f"Getting shop id failed: {e}")
+        # type ignore b/c etsy_python mistyped `message` field as `str`
+        # even though it's `dict[str, Any]` for any successful request that returns data
+        return response.message["shop_id"]  # type: ignore
 
-
-def prep_transaction_data(
-    shop_id: str, data: list[dict[str, Any]]
-) -> list[dict[str, Any]]:
-    transactions: list[dict[str, Any]] = []
-    for receipt in data:
-        if receipt["seller_user_id"] == shop_id:
-            for transaction in receipt["transactions"]:
-                prepped: dict[str, Any] = {}
-                prepped["receipt_id"] = receipt["receipt_id"]
-                prepped["sale_date"] = datetime.fromtimestamp(
-                    receipt["created_timestamp"]
+    def get_sales_data(self) -> list[dict[str, Any]]:
+        receipts: ReceiptResource = ReceiptResource(self.client)
+        offset: int = 0
+        limit: int = 100
+        total: int = -1
+        results: list[dict[str, Any]] = []
+        # don't know total until we get the first response
+        while total == -1 or len(results) < total:
+            try:
+                response: Response = cast(
+                    Response,
+                    receipts.get_shop_receipts(
+                        int(self.shop_id), limit=limit, offset=offset
+                    ),
                 )
-                prepped["transaction_id"] = transaction["transaction_id"]
-                prepped["title"] = transaction["title"]
-                prepped["quantity"] = transaction["quantity"]
-                prepped["listing_id"] = transaction["listing_id"]
-                prepped["product_id"] = transaction["product_id"]
-                prepped["price"] = float(transaction["price"]["amount"]) / (
-                    1.0
-                    if transaction["price"]["divisor"] == 0
-                    else transaction["price"]["divisor"]
-                )
-                transactions.append(prepped)
-    return transactions
+            except RequestException as e:
+                message = f"Failure to get receipts for shop id '{self.shop_id}'\n{offset=}\n{total=}\n{len(results)=}\n{e}"
+                log_and_raise_api_error(message)
+            else:
+                # etsy_python mistyped `message` field as `str`
+                # even though it's `dict[str, Any]` for any successful request that returns data
+                data: dict[str, Any] = cast(dict[str, Any], response.message)
+                total = data["count"]
+                results.extend(data["results"])
+                offset += limit
+        return results
 
+    def prep_transaction_data(self, data: list[dict[str, Any]]) -> list[dict[str, Any]]:
+        transactions: list[dict[str, Any]] = []
+        for receipt in data:
+            if receipt["seller_user_id"] == self.shop_id:
+                for transaction in receipt["transactions"]:
+                    prepped: dict[str, Any] = {}
+                    prepped["receipt_id"] = receipt["receipt_id"]
+                    prepped["sale_date"] = datetime.fromtimestamp(
+                        receipt["created_timestamp"]
+                    )
+                    prepped["transaction_id"] = transaction["transaction_id"]
+                    prepped["title"] = transaction["title"]
+                    prepped["quantity"] = transaction["quantity"]
+                    prepped["listing_id"] = transaction["listing_id"]
+                    prepped["product_id"] = transaction["product_id"]
+                    prepped["price"] = float(transaction["price"]["amount"]) / (
+                        1.0
+                        if transaction["price"]["divisor"] == 0
+                        else transaction["price"]["divisor"]
+                    )
+                    transactions.append(prepped)
+        return transactions
 
-def pull_data(code: str, state: str) -> None:
-    client: EtsyClient = get_authenticated_client(code, state)
-    shop_id: str = get_shop_id(client)
-    data: list[dict[str, Any]] = get_shop_sales_data(client, shop_id)
-    data = prep_transaction_data(shop_id, data)
-    with SCDatabased() as db:
-        db.save_etsy_data(shop_id, data)
+    def pull_data(self) -> None:
+        get_logger().info(f"Pulling sales data for shop '{self.shop_id}'.")
+        data: list[dict[str, Any]] = self.get_sales_data()
+        data = self.prep_transaction_data(data)
+        with SCDatabased() as db:
+            db.save_etsy_data(self.shop_id, data)
+        get_logger().info(f"Retrieved {len(data)} records for shop '{self.shop_id}'.")
--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,13 @@
 import dotenv
+import loggi
 
 # etsy_python doesn't have any typing stub files
-from etsy_python.v3.auth.OAuth import EtsyOAuth  # type: ignore
 from flask import Flask, Response, make_response, request
 from pathier import Pathier
 
 import etsy
 import exceptions
+from etsy import AuthenticatedClient, OAuthProvider
 
 app = Flask(__name__)
 
@@ -25,26 +26,30 @@ def load_content(filename: str) -> str:
     return (pages_dir / filename).read_text(encoding="utf-8")
 
 
+def get_logger() -> loggi.Logger:
+    return loggi.getLogger("app", Pathier(__file__).parent / "logs")
+
+
 @app.route("/")
 def landing():
     response: Response = make_response(load_content("landing.html"))
     code: str | None = request.args.get("code", None)
     state: str = request.args.get("state", "")
     print(state)
-    if code is not None and etsy.state_exists(state):
+    if code is not None and OAuthProvider.state_exists(state):
         try:
-            etsy.pull_data(code, state)
+            AuthenticatedClient.from_redirect(code, state).pull_data()
         except Exception:
             etsy.get_logger().exception("Error pulling shop data from Etsy\n")
             # TODO return different content with error message
+    if code is not None and not OAuthProvider.state_exists(state):
+        get_logger().info(
+            f"Url has code param present ('{code}'), but no valid state param ('{state}')."
+        )
+        # TODO return different content with error message
     return response
 
 
 @app.route("/authurl")
 def get_etsy_auth_url():
-    oauth: EtsyOAuth = etsy.get_oauth()
-    auth_url: str
-    state: str
-    auth_url, state = oauth.get_auth_code()
-    etsy.save_oauth(state, oauth.code_verifier)
-    return auth_url
+    return OAuthProvider.get_auth_url()
